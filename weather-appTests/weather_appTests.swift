bc76619e4726b14eb77f27efb1728d65710449f1
landon r <landon@rohatensky.com>
2023-03-15 12:38:41 -0700

    add tests

--- weather-appTests/weather_appTests.swift
+++ weather-appTests/weather_appTests.swift
@@ -9,7 +9,9 @@ import XCTest
 @testable import weather_app
 
 final class weather_appTests: XCTestCase {
-
+    let woeid: Int = 4418
+    let manager = WeatherManager()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,19 +19,28 @@ final class weather_appTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testLoadsData() {
+        let data = manager.getWeatherData(woeid: woeid)
+        XCTAssertNotNil(data)
+    }
+    
+    func testConsolidatedWeatherExists() {
+        let data = manager.getWeatherData(woeid: woeid)
+        XCTAssert(data?.consolidatedWeather.first?.id == 4805883302248448)
+    }
+    
+    func testWoeIDMatches() {
+        let data = manager.getWeatherData(woeid: woeid)
+        XCTAssert(data?.woeid == woeid)
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
+        let data = manager.getWeatherData(woeid: woeid)
         self.measure {
             // Put the code you want to measure the time of here.
+            let data = manager.getWeatherData(woeid: woeid)
         }
     }
 
